cbde20c0c42c97fbabd8c3e24e7852745c33aaff
szekelyadam <adibadi@Richards-MacBook-Pro.local>
2016-02-21 19:12:51 +0100

    add, multiply, divide and history

--- iCalculator/ViewController.swift
+++ iCalculator/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputTextFieldA: UITextField!
+    @IBOutlet weak var inputTextFieldB: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
+    @IBOutlet weak var operationSelector: UISegmentedControl!
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        operationSelector.selectedSegmentIndex = 0
+        self.textView.editable = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func backgroundTouchUpInside(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    enum OperationType {
+        case Add
+        case Mul
+        case Div
+    }
+    
+    var operationType = OperationType.Add
+    
+    @IBAction func operationSelectorValueChanged(sender: AnyObject) {
+        switch operationSelector.selectedSegmentIndex {
+        case 0:
+            operationType = .Add
+        case 1:
+            operationType = .Mul
+        case 2:
+            operationType = .Div
+        default:
+            operationType = .Add
+        }
+    }
+    
+    @IBAction func calculateButtonTouchUpInside(sender: AnyObject) {
+        let numberFormatter = NSNumberFormatter()
+        
+        view.endEditing(true)
+        
+        if let
+            textA = self.inputTextFieldA.text,
+            textB = self.inputTextFieldB.text,
+            a = numberFormatter.numberFromString(textA)?.doubleValue,
+            b = numberFormatter.numberFromString(textB)?.doubleValue {
+                var result = 0.0
+                var operatorString = ""
+                switch operationType {
+                case OperationType.Add:
+                    result = a + b
+                    operatorString = " + "
+                case OperationType.Mul:
+                    result = a * b
+                    operatorString = " * "
+                case OperationType.Div:
+                    result = a / b
+                    operatorString = " / "
+                }
+                resultLabel.text = "\(result)"
+                let history = "\(a) \(operatorString) \(b) = \(result) \n"
+                textView.text = history + textView.text
+        }
+    }
 }
+
+
+
+
+
 
